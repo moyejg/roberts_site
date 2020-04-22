16b29772bf297481f75b5e69a81268490724c8fb
moyejg <moye.jg@gmail.com>
2020-04-22 12:31:54 -0400

    trying to deploy on heroku

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,9 @@
 const { environment } = require('@rails/webpacker')
 
+environment.loaders.get('sass').use.splice(-1, 0, {
+  loader: 'resolve-url-loader'
+})
+
 const webpack = require('webpack')
 environment.plugins.append('Provide',
   new webpack.ProvidePlugin({
